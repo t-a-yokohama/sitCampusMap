4df38a1ca53a70aabc548e0d7043bca339e71063
t-a-yokohama <ict.engineer@ayuki.yokohama>
2022-05-27 16:36:21 +0900

    restored

--- App.js
+++ App.js
@@ -7,106 +7,30 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import type { Node } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    StyleSheet,
+    Alert,
+    PermissionsAndroid,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import CampusMap from './CampusMap';
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    // Androidに位置情報権限を要求
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <CampusMap></CampusMap>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    container: {
+        flex: 1,
+    },
 });
 
 export default App;
---
+++ CampusMap.js
@@ -0,0 +1,285 @@
+import React, { useState , useEffect } from 'react';
+import type {Node} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    Image,
+    TextInput,
+    Dimensions,
+    TouchableOpacity,
+    Platform,
+} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import LPF from 'lpf/lib/LPF';
+import Modal from 'react-native-modal';
+
+// iOSに位置情報権限を要求
+if (Platform.OS == 'ios') {
+    Geolocation.requestAuthorization("whenInUse");
+}
+
+
+
+const CampusMap: () => Node = () => {
+    // スタイルに使う変数の定義
+    const defaultWidth = 1080;
+    const defaultHeight = 960;
+    const {width, height, scale} = Dimensions.get('window');
+    const imageSource = './images/gas.jpg';
+
+    // 地図画像の表示用変数
+    const [mapWidth, setMapWidth] = useState(defaultWidth);
+    const [mapHeight, setMapHeight] = useState(defaultHeight);
+    const [mapTopY, setMapTopY] = useState(0);
+    const [mapLeftX, setMapLeftX] = useState(0);
+    const [mapDeg, setMapDeg] = useState(0);
+    const [mapScale, setMapScale] = useState(1.0);
+
+    // タッチ操作の直前位置記憶用
+    const [prevPointX1, setPrevPointX1] = useState();
+    const [prevPointY1, setPrevPointY1] = useState();
+    const [prevPointX2, setPrevPointX2] = useState();
+    const [prevPointY2, setPrevPointY2] = useState();
+
+
+    // 方角固定のモード保存用
+    const [mapHeading, setMapHeading] = useState('west');
+
+    // モーダルの表示・非表示切り替え
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const modal = React.createRef();
+
+    
+
+    // スタイルの定義
+    const styles = StyleSheet.create({
+        whole_wrapper: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '200%',
+            width: '200%',
+            backgroundColor: '#444444',
+        },
+        map_layer: {
+            position: 'absolute',
+            top: mapTopY,
+            left: mapLeftX,
+            height: mapHeight,
+            width: mapWidth,
+            transform: [{ rotate: mapDeg + "deg" },
+                        { scale: mapScale},
+                        ],
+        },
+        ui_layer: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: height,
+            width: width,
+        },
+        buildg_text: {
+            color: 'red',
+            fontSize: 20,
+            fontWeight: 'bold',
+        },
+        modal_inside: {
+            backgroundColor: '#ffffffdd',
+            margin: 30,
+            borderRadius: 40,
+        },
+        modal_safety: {
+            marginTop: 40,
+            marginLeft: 20,
+            marginRight: 20,
+        }
+    });
+
+    // 詳細な位置情報を取得(Android:500ms)
+    useEffect(
+        () => {
+            const WatchID = Geolocation.watchPosition(
+                position => {
+                    const {latitude, longitude} = position.coords;
+
+                    // console.log(latitude+' '+longitude);
+                },
+                error => {
+                    console.log(error.code+' '+error.message);
+                },
+                {
+                    enableHighAccuracy: true,
+                    distanceFilter: 0,
+                    forceRequestLocation: true,
+                    interval: 500,
+                    fastestInterval: 500,
+                }
+            );
+
+            return () => {
+                if (WatchID) {
+                    Geolocation.clearWatch(WatchID);
+                }
+            }
+        },
+        []
+    );
+
+
+    // 方位角が更新されたらマップに反映させる処理
+    useEffect(
+        () => {
+            switch (mapHeading) {
+                case 'west':
+                    setMapDeg(90);
+                    break;
+                case 'north':
+                    setMapDeg(0);
+                    break;
+                case 'around':
+                    // setMapDeg(___Heading);
+                    break;
+            }
+        },
+        [/*___Heading,*/mapHeading]
+    );
+
+    // マップへのタッチが始まったときの動作
+    const touchInit = (e) => {
+        e.preventDefault();
+        const touchList = e.nativeEvent.touches;
+        if (touchList.length == 1) setPrevOnePoint(touchList) ;
+        else {
+            // マルチタッチ非対応
+        }
+    };
+
+    // マップ上をドラッグしているときの動作
+    const mapTrans = (e) => {
+        e.preventDefault();
+        const touchList = e.nativeEvent.touches;
+        if (touchList.length == 1) {
+            mapMove(touchList);
+            setPrevOnePoint(touchList);
+        }
+        else {
+            // マルチタッチ非対応
+        }
+    };
+
+    // タッチされた位置の記録・更新
+    const setPrevOnePoint = (touchList) => {
+        if (touchList[0].identifier == 0) {
+            setPrevPointX1(touchList[0].pageX);
+            setPrevPointY1(touchList[0].pageY);
+        }
+        else {
+            setPrevPointX2(touchList[0].pageX);
+            setPrevPointY2(touchList[0].pageY);
+        }
+    };
+
+    // マップ拡大動作
+    const mapZoomIn = () => {
+        if (mapScale < 1.8) {
+            setMapScale(mapScale + 0.2);
+        }
+    };
+
+    // マップ縮小動作
+    const mapZoomOut = () => {
+        if (mapScale > 0.6) {
+            setMapScale(mapScale - 0.2);
+        }
+    };
+
+    // マップ位置のドラッグ移動
+    const mapMove = (touchList) => {
+        const Moved = {x: 0, y: 0};
+
+        if (touchList[0].identifier == 0) {
+            Moved.x = touchList[0].pageX - prevPointX1;
+            Moved.y = touchList[0].pageY - prevPointY1;
+        }
+        else {
+            Moved.x = touchList[0].pageX - prevPointX2;
+            Moved.y = touchList[0].pageY - prevPointY2;
+        }
+
+        setMapLeftX(mapLeftX + Moved.x);
+        setMapTopY(mapTopY + Moved.y);
+    };
+
+    // マップの回転固定切り替え
+    const toggleHeading = () => {
+        switch (mapHeading) {
+            case 'west':
+                setMapHeading('north');
+                break;
+            case 'north':
+                setMapHeading('around');
+                break;
+            case 'around':
+                setMapHeading('west');
+                break;
+        }
+    }
+
+    // モーダルの表示・非表示切り替え
+    const toggleModal = () => {
+        setModalVisible(!modalVisible);
+    };
+
+
+    return (
+        <>
+        <View style={styles.whole_wrapper} onTouchStart={touchInit} onTouchMove={mapTrans}>
+            <Image source={require(imageSource)} style={styles.map_layer}></Image>
+            <View style={styles.map_layer} pointerEvents='box-none'>
+                <View style={{flex: 1}}>
+                <TouchableOpacity onPress={toggleModal} style={{position: 'absolute', top: '10%', left: '20%', transform: [{rotate: (-mapDeg) + 'deg'}, {scale: 1/mapScale}]}}>
+                    <Text style={styles.buildg_text}>本館</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={toggleModal} style={{position: 'absolute', top: '25%', left: '8%', transform: [{rotate: (-mapDeg) + 'deg'}, {scale: 1/mapScale}]}}>
+                    <Text style={styles.buildg_text}>糸山英太郎記念教育センター</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.ui_layer} pointerEvents='box-none'>
+                <TouchableOpacity onPress={toggleHeading} style={{top: height*0.70, left: width*0.85, width: 40, backgroundColor: "white"}}>
+                    <Text style={{fontSize: 40}}>＊</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={mapZoomIn} style={{top: height*0.73, left: width*0.85, width: 40, backgroundColor: "white"}}>
+                    <Text style={{fontSize: 40}}>＋</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={mapZoomOut} style={{top: height*0.75, left: width*0.85, width: 40, backgroundColor: "white"}}>
+                    <Text style={{fontSize: 40}}>ー</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+
+        <View>
+            <TextInput style={{zIndex: 5, elevation: 5, position: 'absolute', borderRadius: 5, top: 30, left: width * 0.05, width: width * 0.9, backgroundColor: 'white', fontSize: 24}} placeholder=" Search"></TextInput>
+        </View>
+
+        <Modal isVisible={modalVisible} animationIn="fadeIn" animationOut="fadeOut" onBackButtonPress={toggleModal} onBackdropPress={toggleModal}>
+            <TouchableOpacity onPress={toggleModal} style={{zIndex: 10, elevation: 10, position: 'absolute', top: 40, left: (width - 120), width: 40,}}>
+                <Text style={{fontSize: 40,}}>×</Text>
+            </TouchableOpacity>
+            <ScrollView ref={modal} style={styles.modal_inside}>
+                <View style={styles.modal_safety}>
+                    <Text>Hello World !</Text>
+                </View>
+            </ScrollView>
+        </Modal>
+        </>
+    );
+};
+
+export default CampusMap;
