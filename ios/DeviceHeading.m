9aa2740ec029744465bff43fe61ceee04bade162
t-a-yokohama <20a6062@sit.shonan-it.ac.jp>
2022-05-31 15:09:51 +0900

    created DeviceHeading.swift, DeviceHeading.m and DeviceHeading.h

---
+++ ios/DeviceHeading.m
@@ -0,0 +1,15 @@
+//
+//  DeviceHeading.m
+//  sitcampusmap
+//
+//  Created by 高橋歩希 on 2022/05/31.
+//
+
+#import <Foundation/Foundation.h>
+#import "DeviceHeading.h"
+
+@implementation DeviceHeading
+
+RCT_EXPORT_MODULE()
+
+@end
